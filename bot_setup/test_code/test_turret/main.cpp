8342aa1ba239eecaa55cdcf45ad4c8dc2b4df44c
redDuckBot <dshillin11799@gmail.com>
2024-09-17 18:47:11 +0000

    Finalized test code for 1DOF turret by adding pusher rod

--- bot_setup/test_code/test_turret/main.cpp
+++ bot_setup/test_code/test_turret/main.cpp
@@ -8,13 +8,18 @@
 int motorPin = 16; //Connected to Relay pin 
 int baseServoPin = 13;
 int laserPin = 26;
-//int pusherPin;
+int pusherPin = 12;
 bool motors_on = false;
 bool laser_on = false;
+bool pusher_rod_engaged = false;
 
-//Pulse width range for servo MG99GR 
-#define MIN 550
-#define MAX 2500
+//Pulse width range for servo MG99GR in microseconds 
+#define MIN_BASE_SERVO 550
+#define MAX_BASE_SERVO 2500
+
+//Pulse width range for MG90S micro servo 
+#define MIN_PUSHER_SERVO 600 
+#define MAX_PUSHER_SERVO 2400
 
 using namespace fmt;
 
@@ -24,7 +29,7 @@ void run_turret_test();
 void display_command_prompts();
 void rotate_turret(int, int, int, int);
 void close_gpio();
-int get_pwm_value_from_angle(int);
+int get_pwm_value_from_angle(int, int, int);
 
 int main()
 {
@@ -43,7 +48,8 @@ int main()
 
 void setup_gpio_pins()
 {
-    gpioServo(baseServoPin, MIN);
+    gpioServo(baseServoPin, MIN_BASE_SERVO);
+    gpioServo(pusherPin, MAX_PUSHER_SERVO);
     gpioSetMode(motorPin, PI_OUTPUT);
     gpioWrite(motorPin, 0);
     gpioSetMode(laserPin, PI_OUTPUT);
@@ -63,12 +69,12 @@ bool check_gpio_initialized()
 
 void run_turret_test()
 {
-    bool running = true;
+    bool running_test = true;
     int command;
 
     display_command_prompts();
 
-    while (running)
+    while (running_test)
     {
         print("Enter command: ");
         std::cin >> command;
@@ -106,11 +112,25 @@ void run_turret_test()
             case 3: //swing turret
                 print("Rotating Turret\n");
                 rotate_turret(0,180,3,175);
-		break;
-            case 4: //Exit Program
+		        break;
+            case 4: //Pusher Rod
+                if (pusher_rod_engaged)
+                {
+                    print("Disengaging pusher rod\n");
+                    pusher_rod_engaged = false;
+                    gpioServo(pusherPin, MAX_PUSHER_SERVO);
+                }
+                else
+                {
+                    print("Engaging pusher rod\n");
+                    pusher_rod_engaged = true;
+                    gpioServo(pusherPin, MIN_PUSHER_SERVO);
+                }
+                break;
+            case 5: //Exit Program
                 print("Exiting Program\n");
                 close_gpio();
-                running = false;
+                running_test = false;
                 break;
             default:
                 print("Not a valid command\n");
@@ -124,8 +144,8 @@ void display_command_prompts()
     print("\t1 --> Toggle shooting Motors\n");
     print("\t2 --> Toggle Laser\n");
     print("\t3 --> Swing Turret\n");
-    print("\t4 --> Exit Testing\n\n\n");
-
+    print("\t4 --> Pusher Rod\n");
+    print("\t5 --> Exit Testing\n\n\n");
 }
 
 /*
@@ -142,7 +162,8 @@ void rotate_turret(int startAngle, int endAngle, int angleIncrement, int pause)
     for (int currAngle = startAngle; currAngle <= endAngle; currAngle += 
         angleIncrement)
     {
-        current_PWM = get_pwm_value_from_angle(currAngle);
+        current_PWM = get_pwm_value_from_angle(currAngle, MAX_BASE_SERVO, 
+            MIN_BASE_SERVO);
         gpioServo(baseServoPin, current_PWM);
         std::this_thread::sleep_for(std::chrono::milliseconds(pause));
     }
@@ -151,30 +172,33 @@ void rotate_turret(int startAngle, int endAngle, int angleIncrement, int pause)
     for (int currAngle = endAngle; currAngle >= startAngle; currAngle -= 
         angleIncrement)
     {
-        current_PWM = get_pwm_value_from_angle(currAngle);
+        current_PWM = get_pwm_value_from_angle(currAngle,MAX_BASE_SERVO,
+            MIN_BASE_SERVO);
         gpioServo(baseServoPin, current_PWM);
         std::this_thread::sleep_for(std::chrono::milliseconds(pause));
     }
 }
 
-int get_pwm_value_from_angle(int angle)
+int get_pwm_value_from_angle(int angle, int max_pulse_width, 
+    int min_pulse_width)
 {
+    //Pulse widths are in microseconds
     double x;
     int pwm_value;
-    double pwm_range = MAX - MIN;
+    double pwm_range = max_pulse_width - min_pulse_width;
 
     x = angle / 180.0;
     x = pwm_range * x;
-    pwm_value = (int) (MIN + x);
+    pwm_value = (int) (min_pulse_width + x);
 
-    if (pwm_value > MAX)
+    if (pwm_value > max_pulse_width)
     {
-        pwm_value = MAX;
+        pwm_value = max_pulse_width;
     }
 
-    if (pwm_value < MIN)
+    if (pwm_value < min_pulse_width)
     {
-        pwm_value = MIN;
+        pwm_value = min_pulse_width;
     }
 
     return pwm_value;
@@ -184,5 +208,6 @@ void close_gpio()
 {
     gpioWrite(motorPin,0);
     gpioWrite(laserPin,0);
+    gpioServo(pusherPin, MAX_PUSHER_SERVO);
     gpioTerminate();
 }
