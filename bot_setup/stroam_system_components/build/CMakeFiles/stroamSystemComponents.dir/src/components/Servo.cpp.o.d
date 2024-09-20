318a4d6d1df2948f6f82935541a5083629fd252d
RedDuckBot <dshillin11799@gmail.com>
2024-09-20 08:18:39 -0600

    Cleaned up project by not including build directories from cmake directories

--- bot_setup/stroam_system_components/build/CMakeFiles/stroamSystemComponents.dir/src/components/Servo.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/stroamSystemComponents.dir/src/components/Servo.cpp.o: \
- /home/redbot/projects/Foam-Ball-Turret-Bot/bot_setup/stroam_system_components/src/components/Servo.cpp \
- /usr/include/stdc-predef.h \
- /home/redbot/projects/Foam-Ball-Turret-Bot/bot_setup/stroam_system_components/include/components/Servo.hpp
