f75dc905a6965277e1953729b9bb4e797f54b828
RedDuckBot <dshillin11799@gmail.com>
2024-10-07 21:00:14 +0000

    Updated remote camera ros2 pkg

--- remote_setup/ros2_ws/src/stroam_camera/stroam_camera/camera_server_view.py
+++ remote_setup/ros2_ws/src/stroam_camera/stroam_camera/camera_server_view.py
@@ -1,13 +1,12 @@
 import cv2, socket, pickle
-import numpy as np 
 
 """Program acts as a UDP server for viewing Stroam's video camera."""
 
-remote_host_ip = "TODO_CHANGE"
+remote_host_ip = "TODO"
 port = 6669
 
 video_server_UDPsocket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-video_server_socket.bind((remote_host_ip,port))
+video_server_UDPsocket.bind((remote_host_ip,port))
 
 print("Video Server ready for Stroam's view")
 while True:
