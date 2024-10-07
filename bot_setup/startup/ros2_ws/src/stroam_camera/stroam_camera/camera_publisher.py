c850b3e11b428ce2bc5a3c3548f55d8fc84bde18
redDuckBot <dshillin11799@gmail.com>
2024-10-07 19:21:12 +0000

    Finished implementing lifecycle node for bot's camera

--- bot_setup/startup/ros2_ws/src/stroam_camera/stroam_camera/camera_publisher.py
+++ bot_setup/startup/ros2_ws/src/stroam_camera/stroam_camera/camera_publisher.py
@@ -9,65 +9,77 @@ import cv2
 
 class CameraNode(LifecycleNode):
     def __init__(self):
-        super().__init__("camer_publisher")
+        super().__init__("camera_publisher")
+
+        self.declare_parameter("camera_name", rclpy.Parameter.Type.STRING)
+        camera_name = self.get_parameter("camera_name").value
+        self.camera_topic_name = camera_name + "_images"
+
         self.imageConverter = CvBridge()
         self.videoIndex = 0
-        self.publisherFrequency = 1.0
         self.camera_publisher_ = None
         self.camera_pub_buffer_num = 10
+        self.publisherFrequency = 1.0 / self.camera_pub_buffer_num
         self.video_capture_ = None
         self.camera_timer_ = None
+        self.timer_running_ = False 
+
+        self.camera_publisher_ = self.create_lifecycle_publisher(Image,
+            self.camera_topic_name, self.camera_pub_buffer_num)
 
         self.get_logger().info("camera node initialized")
     
     #Create ROS2 communications, connect to camera
     def on_configure(self, previous_state: LifecycleState):
         self.get_logger().info("IN on_configure")
-        self.camera_publisher_ = self.create_lifecycle_publisher(Image,"stroam_images",
-                self.camera_pub_buffer_num)
-        self.video_capture = cv2.VideoCapture(videoIndex)
-        self.camera_timer_ = self.create_timer(publisherFrequency, self.publish_image)
+        self.video_capture_ = cv2.VideoCapture(self.videoIndex)
+        #self.video_capture_.set(3,640)
+        #self.video_capture_.set(4,480)
 
         return TransitionCallbackReturn.SUCCESS
 
-    def publish_image(self):
-        got_image_pass, frame = self.video_capture.read()
-
-        if got_image_pass:
-            ros_image = self.imageConverter.cv2_to_imgmsg(frame, encoding="bgr8")
-            self.camera_publisher_.publish(ros_image)
-        else:
-            raise RuntTimeError("Failed to capture image from camera")
-         
     #Enable camera
     def on_activate(self, previous_state: LifecycleState):
         self.get_logger().info("IN on_activate")
+        self.camera_timer_ = self.create_timer(self.publisherFrequency, self.timer_callback)
+        self.timer_running_ = True
         return super().on_activate(previous_state)  
-    
+
     #Disable camera
     def on_deactivate(self, previous_state: LifecycleState):
         self.get_logger().info("IN on_deactivate")
+        self.close_timer() 
         return super().on_deactivate(previous_state)
 
     #Destroy ROS2 communications, disconnect from camera
     def on_cleanup(self, previous_state: LifecycleState):
         self.get_logger().info("IN on_cleanup")
-        self.close_resources()
+        if self.video_capture_:
+            self.video_capture_.release()
 
         return TransitionCallbackReturn.SUCCESS
     
     def on_shutdown(self, previous_state: LifecycleState):
         self.get_logger().info("IN on_shutdown")
-        self.close_resources()
+        if self.video_capture_:
+            self.video_capture_.release()
+        self.close_timer()
+        self.destroy_lifecycle_publisher(self.camera_publisher_)
 
         return TransitionCallbackReturn.SUCCESS
-    
-    #Utility function for on_cleanup & on_shutdown
-    def close_resources():
-        self.destroy_lifecycle_publisher(self.camera_publisher_)
-        self.destroy_timer(self.camera_timer_)
-        self.video_capture.close()
 
+    def close_timer(self):
+        if self.timer_running_:
+            self.destroy_timer(self.camera_timer_)
+            self.timer_running_ = False
+
+    def timer_callback(self):
+        got_image_pass, frame = self.video_capture_.read()
+
+        if got_image_pass:
+            ros_image = self.imageConverter.cv2_to_imgmsg(frame, encoding="bgr8")
+            self.camera_publisher_.publish(ros_image)
+    
 
 def main(args=None):
     rclpy.init(args=args)
