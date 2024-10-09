8eb3856429c9c17a7ae61eb0da2ae647722d033e
redDuckBot <dshillin11799@gmail.com>
2024-10-09 19:58:15 +0000

    Updated ros2 packages stroam_interface and stroam_turret

--- bot_setup/startup/ros2_ws/src/stroam_turret/src/turretNode.cpp
+++ bot_setup/startup/ros2_ws/src/stroam_turret/src/turretNode.cpp
@@ -1,9 +1,12 @@
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_action/rclcpp_action.hpp"
-#include "stroam_interfaces/msg/xbox_controller.hpp"
+#include "stroam_interfaces/action/turret_instruct.hpp"
 #include "turret/turret.hpp"
 
 using turret::Turret;
+using TurretInstruct = stroam_interfaces::action::TurretInstruct;
+using TurretGoalHandle = rclcpp_action::ServerGoalHandle<TurretInstruct>;
+using namespace std::placeholders;
 
 /**
  * @class TurretNode
@@ -14,16 +17,60 @@ class TurretNode : public Node
     public:
         TurretNode() 
         {
+		turret_enabled = false;
 		callback_group_1_ = this->create_callback_group(rclcpp::CallbackGroupType::Reentrant);
+
+		turret_server_ = rclcpp_action::create_server<TurretInstruct>(
+				this,
+				"turret_actions",
+				std::bind(&TuretNode::goal_callback, this, _1, _2),
+				std::bind(&TurretNode::cancel_callback, this, _1),
+				std::bind(&TurretNode::handle_accepted_callback, this, _1),
+				rcl_action_server_get_default_options(),
+				callback_group_1_
+		);
+		
 		RCLCPP_INFO(this->get_logger(), "Turret node initialized");
         }
-
-
 
     private:
         std::unique_ptr<Turret> turret_ = std::make_unique<Turret>();
 
+	bool turret_enabled; 
+
 	rclcpp_CallbackGroup::sharedPtr callback_group_1_;
+	
+	/**
+	 * @brief Handle incoming turret goal from TurretInstruct
+	 */
+	rclcpp_action::GoalResponse:: goal_callback(
+		const rclcpp_action::GoalUUID &uuid, 
+		std::shared_ptr<const TurretInstruct> goal)
+	{
+		(void) uuid;
+		(void) goal;
+		return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
+	}
+
+	/**
+	 * @brief Handle a rejected turret goal request
+	 */
+	rclcpp_action::CancelResponse cancel_callback(
+		const std::shared_ptr<TurretGoalHandle> goal_handle)
+	{
+		(void) goal_handle;
+		return rclcpp_action::CancelResponse::ACCEPT;
+	}
+	
+	/**
+	 * @brief Handle accepted turret goal sent from Stroam manager node
+	 */
+	void handle_accepted_callback(
+		const std::shared_ptr<TurretInstruct> goal_handle)
+	{
+		(void) goal_handle;
+	}
+
 }
 
 
