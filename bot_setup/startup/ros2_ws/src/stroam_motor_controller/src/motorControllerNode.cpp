48eede5b62f2180a035053aec0f6ab1a8877d419
redDuckBot <dshillin11799@gmail.com>
2024-10-10 05:37:34 +0000

    Finished implementing motor controller node

--- bot_setup/startup/ros2_ws/src/stroam_motor_controller/src/motorControllerNode.cpp
+++ bot_setup/startup/ros2_ws/src/stroam_motor_controller/src/motorControllerNode.cpp
@@ -32,7 +32,7 @@ class MotorControllerNode : public rclcpp::Node
             leftMotors_(IN3, IN4, ENB, "leftMotors"),
             rightMotors_(IN1, IN2, ENA, "rightMotors")
         {
-		motor_control_enabled = false;
+		motor_control_enabled_ = false;
 		callback_group_1_ = this->create_callback_group(
             		rclcpp::CallbackGroupType::Reentrant);
 
@@ -41,7 +41,8 @@ class MotorControllerNode : public rclcpp::Node
 				"motor_controller_actions",
 				std::bind(&MotorControllerNode::goal_callback, this, _1, _2),
 				std::bind(&MotorControllerNode::cancel_callback, this, _1),
-				std::bind(&MotorControllerNode::handle_accepted_callback, this, _1),
+				std::bind(&MotorControllerNode::handle_accepted_callback, this, 
+					_1),
 				rcl_action_server_get_default_options(),
 				callback_group_1_
 		);
@@ -53,25 +54,44 @@ class MotorControllerNode : public rclcpp::Node
         MotorDriver leftMotors_; 
         MotorDriver rightMotors_; 
 
-	bool motor_control_enabled; 
+		bool motor_control_enabled_;
+		std::mutex mutex_;
 
-	rclcpp_action::Server<MotorsInstruct>::SharedPtr motor_contr_server_;
-	rclcpp::CallbackGroup::SharedPtr callback_group_1_;
+		std::shared_ptr<MotorsGoalHandle> current_handle_goal_;//motor instructs
+		rclcpp_action::Server<MotorsInstruct>::SharedPtr motor_contr_server_;
+		rclcpp::CallbackGroup::SharedPtr callback_group_1_;
 	
 	/**
 	 * @brief Handle incoming motor controller goal inside MotorsInstruct
+	 *
+	 * A new motor instruction (goal) is accepted provided that a current
+	 * goal isn't being executed, otherwise reject new goal.
 	 */
 	rclcpp_action::GoalResponse goal_callback(
 		const rclcpp_action::GoalUUID &uuid, 
-		std::shared_ptr<const MotorsInstruct::Goal> goal)
+		std::shared_ptr<const MotorsInstruct::Goal> new_motors_goal)
 	{
+		{
+			std::lock_guard<std::mutex> lock(mutex_);
+			if (current_handle_goal_) 
+			{
+				if (current_handle_goal_ -> is_active())
+				{
+					RCLCPP_INFO(this->get_logger(), 
+						"[Motor Server]: A goal is active, rejected new goal");
+					return rclcpp_action::GoalResponse::REJECT;
+				}
+			}
+		}
+
 		(void) uuid;
-		(void) goal;
+		(void) new_motors_goal;
 		return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
 	}
 
 	/**
 	 * @brief Handle a rejected motor controller goal request
+	 * 
 	 */
 	rclcpp_action::CancelResponse cancel_callback(
 		const std::shared_ptr<MotorsGoalHandle> goal_handle)
@@ -81,14 +101,97 @@ class MotorControllerNode : public rclcpp::Node
 	}
 	
 	/**
-	 * @brief Handle accepted motor controller goal sent from Stroam manager node
+	 * @brief Handle accepted motor goal sent from Stroam manager node
+	 *@param new_goal_handle the latest accepted motor instructions
 	 */
 	void handle_accepted_callback(
-		const std::shared_ptr<MotorsGoalHandle> goal_handle)
+		const std::shared_ptr<MotorsGoalHandle> new_goal_handle)
 	{
-		(void) goal_handle;
+		{
+			std::lock_guard<std::mutex> lock(mutex_);
+			this -> current_handle_goal_ = new_goal_handle;
+		}
+
+		execute_motor_goal_(new_goal_handle);
+		send_goal_result_(new_goal_handle);
+
 	}
 
+	/**
+	 *@brief Set left and right motor efforts and directions
+	 * 
+	 * @param goal_handle the action containing motor joy-stick values
+	 */
+	void execute_motor_goal_(const std::shared_ptr<MotorsGoalHandle> 
+		goal_handle)
+	{
+
+		double new_left_motors_effort, new_right_motors_effort;
+
+		auto goal = goal_handle -> get_goal();
+		float left_joy_y = goal -> left_joy_stick_y;
+		float right_joy_y = goal -> right_joy_stick_y; 
+		
+		MotorDirection left_joy_dir = MotorDirection::Forward;
+		MotorDirection right_joy_dir = MotorDirection::Forward;
+		
+		//Set left joy stick direction
+		if (left_joy_y < 0)
+		{
+			left_joy_dir = MotorDirection::Forward;
+		} 
+		else 
+		{
+			left_joy_dir = MotorDirection::Backward;
+		}
+
+		//Set right joy stick direction
+		if (right_joy_y < 0)
+		{
+			right_joy_dir = MotorDirection::Forward;
+		}
+		else
+		{
+			right_joy_dir = MotorDirection::Backward;
+		}
+
+		//Check for change in motor directions and set them
+		if (leftMotors_.getDirection() != left_joy_dir)
+		{
+			leftMotors_.setDirection(left_joy_dir);
+		}
+		if (rightMotors_.getDirection() != right_joy_dir)
+		{
+			rightMotors_.setDirection(right_joy_dir);
+		}
+
+		new_left_motors_effort = controllerInput_to_motorEffort(
+			abs(left_joy_y));
+		new_right_motors_effort = controllerInput_to_motorEffort(
+			abs(right_joy_y));
+
+		leftMotors_.setEffortPercent(new_left_motors_effort);
+		rightMotors_.setEffortPercent(new_right_motors_effort);
+	}
+
+	/**
+	 *@brief Take a joy-stick value and convert it to a percentage [0,1]
+	 *@return the percentage or effort
+	 */
+	double controllerInput_to_motorEffort(double joy_stick_input) 
+	{
+		return joy_stick_input * 100;
+	}
+
+	/**
+	 *@brief Send back the result of accepted and executed motor goal
+	 */
+	void send_goal_result_(const std::shared_ptr<MotorsGoalHandle> goal_handle)
+	{
+		auto result = std::make_shared<MotorsInstruct::Result>();
+		result->result = "Success";
+		goal_handle->succeed(result);
+	}
 };
 
 
